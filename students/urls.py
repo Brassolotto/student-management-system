62c1a7a6c95f5ae59e31b4df1a48775cd299e6d7
Ricardo Brassolotto <23336979+Brassolotto@users.noreply.github.com>
2025-03-06 19:37:36 -0300

    Update student URLs and enhance index template layout

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'students'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:id>/edit/', views.edit_student, name='edit'),
+    path('<int:id>/delete/', views.delete_student, name='delete'),
 ]
